4a5b215783dd37126acb38b189de99efc58436dc
Benj Sicam <bsicam@reekoh.com>
2015-11-16 19:20:28 +0800

    Optimizations

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 'use strict';
 
-var platform = require('./platform'),
-	apn = require('apn'),
-	async = require('async'),
+var apn      = require('apn'),
+	platform = require('./platform'),
 	connection;
-
 
 /*
  * Listen for the data event.
  */
 platform.on('data', function (data) {
-
 	var note = new apn.notification();
 	note.setAlertText(data.msg);
 	note.badge = 1;
@@ -20,28 +17,12 @@ platform.on('data', function (data) {
 	//data.token = single device ID
 	//data.tokens = array of device IDs
 	connection.pushNotification(note, data.tokens);
-
-	connection.on('error', function (error) {
-		platform.handleException(error);
-	});
-
-	connection.on('socketError', function (error) {
-		platform.handleException(error);
-	});
-
-	connection.on('transmissionError', function (errorCode, notification, device) {
-
-		platform.handleException(new Error('Error: ' + errorCode + ' Notification: ' + notification + ' Device: ' + device));
-
-	});
-
 });
 
 /*
  * Event to listen to in order to gracefully release all resources bound to this service.
  */
 platform.on('close', function () {
-
 	var domain = require('domain');
 	var d = domain.create();
 
@@ -54,35 +35,16 @@ platform.on('close', function () {
 		connection.shutdown();
 		platform.notifyClose();
 	});
-
-
 });
 
 /*
  * Listen for the ready event.
  */
 platform.once('ready', function (options) {
+	if (options.production === null || options.production === undefined || options.production === '')
+		options.production = false;
 
-	var connection_options = {
-		cer: options.certData,
-		key: options.keyData
-	};
-
-    if (options.ca)
-	   connection_options.ca = options.ca;
-
-	if (options.passphrase)
-		connection_options.passphrase = options.passphrase;
-
-	if (options.port)
-		connection_options.port = options.port;
-
-	connection = apn.connection(connection_options);
-
-	connection.on('connected', function () {
-		platform.log('APNS Connector has initialized');
-		platform.notifyReady();
-	});
+	connection = apn.connection(options);
 
 	connection.on('error', function (error) {
 		platform.handleException(error);
@@ -92,10 +54,16 @@ platform.once('ready', function (options) {
 		platform.handleException(error);
 	});
 
-	connecrtion.on('cacheTooSmall', function (sizeDiff) {
-		platform.handleException(new Error('Error: Cache size is too small'));
+	connection.on('transmissionError', function (errorCode, notification, device) {
+		platform.handleException(new Error('Error: ' + errorCode + ' Notification: ' + notification + ' Device: ' + device));
 	});
 
+	connection.on('cacheTooSmall', function (sizeDiff) {
+		platform.handleException(new Error('Error: Cache size is too small. Size diff: ' + sizeDiff));
+	});
 
-
+	connection.on('connected', function () {
+		platform.log('APNS Connector has initialized');
+		platform.notifyReady();
+	});
 });
