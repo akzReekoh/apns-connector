7432c9a53bd47b5b6d8cd227affff196681a72a1
Benj Sicam <bsicam@reekoh.com>
2015-11-16 19:43:00 +0800

    Optimizations and more validations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var apn      = require('apn'),
+var _        = require('lodash'),
+	apn      = require('apn'),
 	platform = require('./platform'),
 	connection;
 
@@ -8,14 +9,21 @@ var apn      = require('apn'),
  * Listen for the data event.
  */
 platform.on('data', function (data) {
-	var note = new apn.notification();
-	note.setAlertText(data.msg);
-	note.badge = 1;
+	if (_.isEmpty(data.title))
+		return platform.handleException(new Error('Missing data parameter: title'));
 
-	//NOTE:
-	//a single token or an array of tokens can be passed
-	//data.token = single device ID
-	//data.tokens = array of device IDs
+	if (_.isEmpty(data.message))
+		return platform.handleException(new Error('Missing data parameter: message'));
+
+	if (_.isEmpty(data.tokens))
+		return platform.handleException(new Error('Missing data parameter: tokens'));
+
+	var note = new apn.Notification();
+
+	note.setAlertTitle(data.title);
+	note.setAlertText(data.message);
+	note.setBadge(1);
+
 	connection.pushNotification(note, data.tokens);
 });
 
@@ -55,15 +63,23 @@ platform.once('ready', function (options) {
 	});
 
 	connection.on('transmissionError', function (errorCode, notification, device) {
-		platform.handleException(new Error('Error: ' + errorCode + ' Notification: ' + notification + ' Device: ' + device));
+		platform.handleException(new Error('Notification caused error: ' + errorCode + ' for device ' + device + '. Notification: ' + notification));
 	});
 
 	connection.on('cacheTooSmall', function (sizeDiff) {
 		platform.handleException(new Error('Error: Cache size is too small. Size diff: ' + sizeDiff));
 	});
 
+	connection.on('timeout', function () {
+		platform.handleException(new Error('Connection timeout.'));
+	});
+
+	connection.on('disconnected', function () {
+		platform.handleException(new Error('Disconnected from server.'));
+	});
+
 	connection.on('connected', function () {
-		platform.log('APNS Connector has initialized');
+		platform.log('APNS Connector initialized.');
 		platform.notifyReady();
 	});
 });
